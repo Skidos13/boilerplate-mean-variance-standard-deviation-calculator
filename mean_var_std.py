01c5005318aec87c1b6b907231a88ffb7d22dc90
Andrei Chiran <a.chiran13@yahoo.com>
2024-10-30 15:54:23 +0000

    initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
+ef calculate(list:list):
+    #check if list is not 9 elements
+    if len(list)!=9:
+        raise ValueError("List must contain nine numbers.")
+    #convert list to 3x3 numpy array
+    array=np.array(list).reshape(3,3)
+    mean=[array.mean(axis=0).tolist(),array.mean(axis=1).tolist(),array.mean().tolist()]
+    variance=[array.var(axis=0).tolist(),array.var(axis=1).tolist(),array.var().tolist()]
+    standard_deviation=[array.std(axis=0).tolist(),array.std(axis=1).tolist(),array.std().tolist()]
+    minimum=[array.min(axis=0).tolist(),array.min(axis=1).tolist(),array.min().tolist()]
+    maximum=[array.max(axis=0).tolist(),array.max(axis=1).tolist(),array.max().tolist()]
+    sums=[array.sum(axis=0).tolist(),array.sum(axis=1).tolist(),array.sum().tolist()]
 
-def calculate(list):
+    #calculate mean
+    
+    return {"mean":mean,"variance":variance,"standard deviation":standard_deviation,"max":maximum,"min":minimum,"sum":sums}
 
-
-
-
-    return calculations
